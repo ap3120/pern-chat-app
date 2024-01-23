56de6d59e9ff03a3876ff2ef8c073798ade8c4ea
dev <ygbe@hotmail.fr>
2024-01-23 22:37:40 +0100

    add login test

--- frontend/cypress/e2e/home-page.cy.js
+++ frontend/cypress/e2e/home-page.cy.js
@@ -1,17 +1,29 @@
 describe('Login page', () => {
-  it('Successfully loads', () => {
+  it('successfully loads the login page', () => {
     cy.visit('/')
     cy.contains('Register here').click()
     cy.url().should('include', '/register')
   })
 
-  it('Switch theme', () => {
+  it('switches theme', () => {
     cy.visit('/')
     
   })
 
-  it('Type username', () => {
-    cy.get('#username').type('Baldwin')
+  it('tries to loggin', () => {
+    cy.visit('/')
+    cy.get('[id="username"]').type('Baldwin')
     cy.get('[id="username"]').should('have.value', 'Baldwin')
+    cy.get('[id="password"]').type('wrongpassword')
+    cy.get('[id="password"]').should('have.value', 'wrongpassword')
+
+    cy.get('[id="login-btn"]').click();
+    cy.get('[id="helper-text"]').should('be.visible')
+
+    cy.get('[id="password"]').clear().type(Cypress.env('baldwin_password'))
+    cy.get('[id="password"]').should('have.value', Cypress.env('baldwin_password'))
+    cy.get('[id="login-btn"]').click();
+    cy.url().should('include', '/dashboard')
+    
   })
 })
