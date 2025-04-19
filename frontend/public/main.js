06d6c16bea94298a27b9b4addb477a751181258e
dev <ygbe@hotmail.fr>
2025-04-19 16:38:55 +0200

    install electron

---
+++ frontend/public/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+    autoHideMenuBar: true,
+  })
+
+  //win.loadFile('public/index.html')
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'build/index.html'));
+  }
+
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong")
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    app.quit()
+  })
+})
