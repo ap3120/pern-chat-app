b3f64e5aedff9abb2b8288039fb8eaee63ea0a82
dev <ygbe@hotmail.fr>
2025-04-19 17:03:20 +0200

    merge master into playwrighttest

---
+++ frontend/public/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+    autoHideMenuBar: true,
+  })
+
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'build/index.html'));
+  }
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    app.quit()
+  })
+})
